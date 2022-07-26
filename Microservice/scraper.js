5d69cf5a56b514ea51bda06da560fc266da6981e
ebabcock93 <86132170+ebabcock93@users.noreply.github.com>
2022-07-25 23:52:00 -0400

    Add files via upload

---
+++ Microservice/scraper.js
@@ -0,0 +1,30 @@
+const PORT = 3000
+import express from 'express';
+import usetube from 'usetube';
+
+const app = express()
+
+app.get('/', async (req, res) => {
+    
+    const data = await usetube.searchVideo('Bicycle Repairs');
+
+    var info = data.videos;
+
+    const array = []
+    
+    for (let i = 0; i < info.length; i++) {
+        var curr_dict = info[i]
+        var ids = curr_dict.id
+        var titles = curr_dict.original_title
+        array.push({
+            ids,
+            titles  
+      })
+    
+    }
+
+    res.send(array);
+    
+    })
+
+app.listen(PORT, () => {console.log(`server running on PORT ${PORT}`)})
